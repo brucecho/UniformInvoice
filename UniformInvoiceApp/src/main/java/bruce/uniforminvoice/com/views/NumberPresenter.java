66f82270b4068dd7940df15467bca1c170e23588
brucecho <brucecho38@gmail.com>
2018-04-03 09:00:00 +0800

    20180403

--- UniformInvoiceApp/src/main/java/bruce/uniforminvoice/com/views/NumberPresenter.java
+++ UniformInvoiceApp/src/main/java/bruce/uniforminvoice/com/views/NumberPresenter.java
@@ -6,6 +6,7 @@ import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import bruce.uniforminvoice.com.UniformInvoice;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class NumberPresenter {
@@ -14,25 +15,30 @@ public class NumberPresenter {
     private View number;
 
     @FXML
+    private ComboBox cbxMonth;
+    
+    @FXML
     private Label label;
 
     public void initialize() {
         number.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = MobileApplication.getInstance().getAppBar();
-                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
-                        MobileApplication.getInstance().showLayer(UniformInvoice.MENU_LAYER)));
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e
+                        -> MobileApplication.getInstance().showLayer(UniformInvoice.MENU_LAYER)));
                 appBar.setTitleText("Number");
-                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> 
-                        System.out.println("Search")));
+                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e
+                        -> System.out.println("Search")));
                 appBar.setVisible(false);
             }
         });
+
+        this.cbxMonth.getItems().addAll("106-1112","107-0102");
     }
-    
+
     @FXML
-    void buttonClick() {
-        label.setText("Hello JavaFX Universe!");
+    void cbxMonthSelect() {
+        label.setText(this.cbxMonth.getSelectionModel().getSelectedItem().toString());
     }
-    
+
 }
